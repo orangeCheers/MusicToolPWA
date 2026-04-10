e9618de33beba1a3616b36f22a378763955f8b38
ystqingkaicheng <chengqingkai@ysten.com>
2026-04-10 14:49:33 +0800

    Initial commit: Music Tool PWA

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'music-tool-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['./index.html', './manifest.json']);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
